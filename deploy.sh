58fc7a877df501f4281d70a2a68fff522cf28c88
Makoto <inouemak@googlemail.com>
2019-07-18 09:54:01 +0100

    Add deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,21 @@
-#!/usr/bin/env sh
+#!/bin/bash
+# From https://medium.com/onfido-tech/travis-surge-github-auto-deploy-every-pr-branch-and-tag-a6c8c790831f
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  echo "Deploying PR branch to surge..."
+else
+  echo "Not running on PR, skipping."
+  exit -1
+fi
 
-# abort on errors
-set -e
-
-# build
-# vuepress build
-npm run build
-
-# navigate into the build output directory
-cd .vuepress/dist
-
-# include the custom domain for the GitHub Page
-cp ../.github/CNAME .
-
-# include the README for the banch
-cp -r ../.github .
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:JosefJ/PragueBlockchainWeek.git master:gh-pages
-
-cd -
+REPO_SLUG_ARRAY=(${TRAVIS_REPO_SLUG//\// })
+REPO_OWNER=${REPO_SLUG_ARRAY[0]}
+REPO_NAME=${REPO_SLUG_ARRAY[1]}
+DEPLOY_PATH=./.vuepress/dist
+DEPLOY_SUBDOMAIN=pr${TRAVIS_PULL_REQUEST}
+DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
+echo $DEPLOY_DOMAIN
+cp ${DEPLOY_PATH}/index.html ${DEPLOY_PATH}/200.html
+surge -p ${DEPLOY_PATH} -d $DEPLOY_DOMAIN
+GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+curl -u "noblocknoparty-devs:${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"PR deployed at: '${DEPLOY_DOMAIN}'"}'
