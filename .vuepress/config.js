7078c334c2f77a5542ecfffe2448cf95c3580245
Josef Je <jelacic.josef@gmail.com>
2022-05-10 18:10:02 +0200

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -21,7 +21,7 @@ module.exports = {
       },
       {
         text: 'Submit an Event',
-        link: 'https://goo.gl/forms/zYvjmpEsfeM1KpRt2'
+        link: 'https://cf23uhv4kuq.typeform.com/to/pVgp6whd'
       }
     ],
     logo: 'logo.png',
